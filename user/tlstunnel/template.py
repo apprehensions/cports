b33374ffa5901f59a75f0bfec581514eb2a6c276
sewn <sewn@disroot.org>
2026-03-18 17:54:19 +0000

    user/tlstunnel: upgrade to 0.5.0

--- user/tlstunnel/template.py
+++ user/tlstunnel/template.py
@@ -1,6 +1,6 @@
 pkgname = "tlstunnel"
-pkgver = "0.4.0"
-pkgrel = 6
+pkgver = "0.5.0"
+pkgrel = 0
 build_style = "go"
 make_build_args = [
     "-ldflags= "
@@ -14,7 +14,7 @@ pkgdesc = "TLS reverse proxy"
 license = "MIT"
 url = "https://codeberg.org/emersion/tlstunnel"
 source = f"{url}/archive/v{pkgver}.tar.gz"
-sha256 = "787d39adf16f1f57dde002286c7d32fe75da50db3e833a92f005c4effff5cd3f"
+sha256 = "3eb0a3030aa9c2d955766705d37a3590032c71fcc656bcdf4644d88031dc4571"
 file_modes = {
     "usr/bin/tlstunnel": ("root", "root", 0o755),
 }
